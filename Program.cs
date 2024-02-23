52defedd06922f7e7bad190b72cc2fd949380a7f
Jack Edge <js77js@yahoo.co.uk>
2024-02-23 11:06:51 +0000

    debugging MakeChange()

--- Program.cs
+++ Program.cs
@@ -115,10 +115,15 @@ static void LoadTillEachMorning(int[,] registerDailyStartingCash, int[] cashTill
 
 static void MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int fives = 0, int ones = 0)
 {
-    cashTill[3] += twenties;
-    cashTill[2] += tens;
-    cashTill[1] += fives;
-    cashTill[0] += ones;
+    // cashTill[3] += twenties;
+    // cashTill[2] += tens;
+    // cashTill[1] += fives;
+    // cashTill[0] += ones;
+    int availableTwenties = cashTill[3] + twenties;
+    int availableTens = cashTill[2] + tens;
+    int availableFives = cashTill[1] + fives;
+    int availableOnes = cashTill[0] + ones;
+
 
     int amountPaid = twenties * 20 + tens * 10 + fives * 5 + ones;
     int changeNeeded = amountPaid - cost;
@@ -128,30 +133,30 @@ static void MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int
 
     Console.WriteLine("Cashier prepares the following change:");
 
-    while ((changeNeeded > 19) && (cashTill[3] > 0))
+    while ((changeNeeded > 19) && (availableTwenties > 0))
     {
-        cashTill[3]--;
+        availableTwenties--;
         changeNeeded -= 20;
         Console.WriteLine("\t A twenty");
     }
 
-    while ((changeNeeded > 9) && (cashTill[2] > 0))
+    while ((changeNeeded > 9) && (availableTens > 0))
     {
-        cashTill[2]--;
+        availableTens--;
         changeNeeded -= 10;
         Console.WriteLine("\t A ten");
     }
 
-    while ((changeNeeded > 4) && (cashTill[1] > 0))
+    while ((changeNeeded > 4) && (availableFives > 0))
     {
-        cashTill[1]--;
+        availableFives--;
         changeNeeded -= 5;
         Console.WriteLine("\t A five");
     }
 
-    while ((changeNeeded > 0) && (cashTill[0] > 0))
+    while ((changeNeeded > 0) && (availableOnes > 0))
     {
-        cashTill[0]--;
+        availableOnes--;
         changeNeeded -= 1;
         Console.WriteLine("\t A one");
     }
@@ -159,6 +164,10 @@ static void MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int
     if (changeNeeded > 0)
         throw new InvalidOperationException("InvalidOperationException: The till is unable to make change for the cash provided.");
 
+    cashTill[0] = availableOnes;
+    cashTill[1] = availableFives;
+    cashTill[2] = availableTens;
+    cashTill[3] = availableTwenties;
 }
 
 static void LogTillStatus(int[] cashTill)
